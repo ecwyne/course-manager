69207b8eec9679e14e217a1f562602ccc1cc6823
Eric Wyne <ecwyne@gmail.com>
2017-07-09 16:37:32 -0700

    test CoursesManagerCreatorModal

--- components/CoursesManager/components/CoursesManagerCreator.js
+++ components/CoursesManager/components/CoursesManagerCreator.js
@@ -26,58 +26,65 @@ const mutation = gql`
 	}
 `;
 
-export class CoursesManagerCreator extends React.PureComponent{
-	render() {
-		const {modalShown, setModalShown, fields = {}, fieldHandler, setInFlight, errors = [], setErrors, runQuery} = this.props;
-		const {subject = '', courseNumber = '', description = ''} = fields;
-		const courseNumberValidationState = (courseNumber && !/\b\d\d\d\b/.test(courseNumber)) || ![0,3].includes(courseNumber.length) ? 'error' : null;
-		const disabled = Boolean(courseNumberValidationState) || Object.values(fields).some(R.equals(''));
+export const CoursesManagerCreatorModal = ({fields, fieldHandler, errors = [], setInFlight, setModalShown, runQuery, setErrors}) => {
+	const {subject = '', courseNumber = '', description = ''} = fields;
+	const courseNumberValidationState = (courseNumber && !/\b\d\d\d\b/.test(courseNumber)) || ![0,3].includes(courseNumber.length) ? 'error' : null;
+	const disabled = Boolean(courseNumberValidationState) || Object.values(fields).some(R.equals(''));
 
-		const submit = async () => {
-			setInFlight(true);
-			try {
-				await apolloClient.mutate({mutation, variables: {course: fields}});
-				setInFlight(false);
-				setModalShown(false);
-				runQuery();
-			} catch (err){
-				setErrors(err.graphQLErrors.map(R.prop('message')));
-				console.log({err});
-			}
-		};
-		return (
-			<Button block bsStyle="success" onClick={() => setModalShown(true)}>
-				Add New Course
-				<Modal show={modalShown} onHide={() => setModalShown(false)}>
-					<div ref={ref => this.modalRef = ref}>
-						<Modal.Header closeButton>
-							<Modal.Title>Add New Course</Modal.Title>
-						</Modal.Header>
-						<Modal.Body>
-							<FormGroup controlId="subject">
-								<ControlLabel>Subject</ControlLabel>
-								<FormControl type="text" placeholder="Subject" value={subject} onChange={fieldHandler('subject')}/>
-							</FormGroup>
-							<FormGroup controlId="courseNumber" validationState={courseNumberValidationState}>
-								<ControlLabel>Course Number</ControlLabel>
-								<FormControl type="text" placeholder="Course Number" value={courseNumber} onChange={fieldHandler('courseNumber')}/>
-								<HelpBlock>Must be a zero-filled three digit number (eg. 033)</HelpBlock>
-							</FormGroup>
-							<FormGroup controlId="description">
-								<ControlLabel>Course Description</ControlLabel>
-								<FormControl type="text" placeholder="Course Description" value={description} onChange={fieldHandler('description')}/>
-							</FormGroup>
-							{errors.map(msg => <Alert key={msg} bsStyle="warning">{msg}</Alert>)}
-						</Modal.Body>
+	const submit = async () => {
+		setInFlight(true);
+		try {
+			await apolloClient.mutate({mutation, variables: {course: fields}});
+			setInFlight(false);
+			setModalShown(false);
+			runQuery();
+		} catch (err){
+			setErrors(err.graphQLErrors.map(R.prop('message')));
+			console.log({err});
+		}
+	};
 
-						<Modal.Footer>
-							<Button bsStyle="primary" onClick={submit} disabled={disabled}>Submit</Button>
-						</Modal.Footer>
-					</div>
-				</Modal>
-			</Button>
-		);
-	}
-}
+	return (
+		<div>
+			<Modal.Header closeButton>
+				<Modal.Title>Add New Course</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<FormGroup controlId="subject">
+					<ControlLabel>Subject</ControlLabel>
+					<FormControl type="text" placeholder="Subject" value={subject} onChange={fieldHandler('subject')}/>
+				</FormGroup>
+				<FormGroup controlId="courseNumber" validationState={courseNumberValidationState}>
+					<ControlLabel>Course Number</ControlLabel>
+					<FormControl type="text" placeholder="Course Number" value={courseNumber} onChange={fieldHandler('courseNumber')}/>
+					<HelpBlock>Must be a zero-filled three digit number (eg. 033)</HelpBlock>
+				</FormGroup>
+				<FormGroup controlId="description">
+					<ControlLabel>Course Description</ControlLabel>
+					<FormControl type="text" placeholder="Course Description" value={description} onChange={fieldHandler('description')}/>
+				</FormGroup>
+				{errors.map(msg => <Alert key={msg} bsStyle="warning">{msg}</Alert>)}
+			</Modal.Body>
+
+			<Modal.Footer>
+				<Button bsStyle="primary" onClick={submit} disabled={disabled}>Submit</Button>
+			</Modal.Footer>
+		</div>
+	);
+};
+
+export const CoursesManagerCreator = props => {
+	const {modalShown, setModalShown} = props;
+
+	return (
+		<Button block bsStyle="success" onClick={() => setModalShown(true)}>
+			Add New Course
+			<Modal show={modalShown} onHide={() => setModalShown(false)}>
+				<CoursesManagerCreatorModal {...props}/>
+			</Modal>
+		</Button>
+	);
+};
+
 
 export default enhance(CoursesManagerCreator);
--- components/CoursesManager/components/CoursesManagerCreator.test.js
+++ components/CoursesManager/components/CoursesManagerCreator.test.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import {mount, ReactWrapper} from 'enzyme';
-import {CoursesManagerCreator} from './CoursesManagerCreator.js';
+import {mount} from 'enzyme';
+import {CoursesManagerCreator, CoursesManagerCreatorModal} from './CoursesManagerCreator.js';
 
 
 test('CoursesManagerCreator shown when clicked', () => {
 
 	const setModalShown = jest.fn();
-	const addButton = mount(<CoursesManagerCreator setModalShown={setModalShown} fieldHandler={() => {}}/>);
+	const wrapper = mount(<CoursesManagerCreator setModalShown={setModalShown} fieldHandler={() => {}}/>);
 
-	addButton.find('button').first().simulate('click');
+	wrapper.find('button').first().simulate('click');
 	expect(setModalShown.mock.calls[0][0]).toBe(true);
 });
 
-it('CoursesManagerCreator hidden when clicked', () => {
-	const setModalShown = jest.fn();
-	const modalRef = mount(<CoursesManagerCreator modalShown={true} setModalShown={setModalShown} fieldHandler={() => {}}/>).instance().modalRef;
-	const modal = new ReactWrapper(modalRef, true);
-	modal.find('.close').simulate('click');
-	expect(setModalShown.mock.calls[0][0]).toBe(false);
+test('CoursesManagerCreatorModal validates courseNumber', () => {
+	const wrapper = mount(<CoursesManagerCreatorModal fields={{subject: 'BIO', courseNumber: '12', description: 'some description'}} fieldHandler={() => () => {}}/>);
+	expect(wrapper.find('FormGroup').at(1).prop('validationState')).toBe('error');
+	expect(wrapper.find('Button').first().prop('disabled')).toBe(true);
+});
+
+test('CoursesManagerCreatorModal requires description', () => {
+	const wrapper = mount(<CoursesManagerCreatorModal fields={{subject: 'BIO', courseNumber: '101', description: ''}} fieldHandler={() => () => {}}/>);
+	expect(wrapper.find('FormGroup').at(1).prop('validationState')).toBe(null);
+	expect(wrapper.find('Button').first().prop('disabled')).toBe(true);
+});
+
+test('CoursesManagerCreatorModal enabled with all fields filled', () => {
+	const wrapper = mount(<CoursesManagerCreatorModal fields={{subject: 'BIO', courseNumber: '001', description: 'some description'}} fieldHandler={() => () => {}}/>);
+	expect(wrapper.find('FormGroup').at(1).prop('validationState')).toBe(null);
+	expect(wrapper.find('Button').first().prop('disabled')).toBe(false);
 });
